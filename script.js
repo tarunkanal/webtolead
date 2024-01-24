59fe66d7280b313e6d5344e15d9335e24984c165
tarunkanal <tarunkanal@gmail.com>
2024-01-25 10:40:52 +1100

    recaptcha changes

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function beforesubmit(){
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-AU");
     outputdate.value=formattedDate;
 }
+
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } 
+} 
+setInterval(timestamp, 500); 
